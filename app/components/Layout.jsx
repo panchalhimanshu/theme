586fcee69856743604f5fce82e17c39b3baf72d3
Kahna <himanshupanchal6941@gmail.com>
2024-10-26 16:18:09 +0530

    Update Layout.jsx

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -139,31 +139,31 @@ const Layout = ({ children }) => {
     };
 
     return (
-        <div className={`flex min-h-screen ${theme === 'dark' ? 'bg-[#29303d] text-white' : 'bg-gray-50 text-black'}`}>
-            <aside className={`fixed left-0 shadow top-0 h-full transition-all duration-300 ${theme === 'dark' ? 'bg-[#29303d] text-white' : 'bg-gray-50 text-gray-800'} font-semibold border border-t-0 border-gray-300 ${isSidebarOpen ? 'w-64' : 'w-18'}`}>
-                <div className="p-4 pb-6 text-[#836bfa] text-center text-2xl font-bold border border-r-0 border-gray-300">
+        <div className={`flex min-h-screen dark:bg-black dark:text-white  bg-white text-black'`}>
+            <aside className={`fixed left-0 shadow top-0 h-full transition-all duration-300 dark:bg-black dark:text-white  bg-white text-gray-80'} font-semibold border border-t-0 border-gray-300 ${isSidebarOpen ? 'w-64' : 'w-18'}`}>
+                <div className="p-4 pt-5  dark:text-white text-black text-center text-2xl font-bold border border-r-0 border-gray-300">
                     {isSidebarOpen ? 'Dashboard' : 'D'}
                 </div>
                 <nav className="mt-10">
                     <ul>
-                        {roleId === 3 && <AdminMenu isActive={isActive} isSidebarOpen={isSidebarOpen} toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} hoveredMenu={hoveredMenu} handleMenuHover={handleMenuHover} handleMenuLeave={handleMenuLeave} />}
-                        {roleId === 4 && <StationMenu isActive={isActive} isSidebarOpen={isSidebarOpen} />}
-                        {roleId === 5 && <WarehouseMenu isActive={isActive} isSidebarOpen={isSidebarOpen} />}
+                    {roleId == 3 && <AdminMenu isActive={isActive} isSidebarOpen={isSidebarOpen} toggleMenu={toggleMenu} isMenuOpen={isMenuOpen} hoveredMenu={hoveredMenu} handleMenuHover={handleMenuHover} handleMenuLeave={handleMenuLeave} />}
+                        {roleId == 4 && <StationMenu isActive={isActive} isSidebarOpen={isSidebarOpen} />}
+                        {roleId == 5 && <WarehouseMenu isActive={isActive} isSidebarOpen={isSidebarOpen} />}
                     </ul>
                 </nav>
             </aside>
 
             <div className={`flex flex-col flex-grow transition-all duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-[70px]'}`}>
-                <header className={`${theme === 'dark' ? 'bg-[#29303d] text-white' : 'bg-white text-black'} shadow p-4 pb-3 border border-l-0 border-gray-300 py-5 sticky top-0 z-50`}>
-                    <div className="max-w-7xl mx-auto flex justify-between items-center">
+                <header className={`dark:bg-black dark:text-white  bg-white text-black hadow p-4 pb-3 border border-l-0 border-gray-300  sticky top-0 z-50`}>
+                    <div className=" flex justify-between items-center">
                         <div className="flex items-center">
-                            <button className="text-gray-600 dark:text-gray-400 focus:outline-none" onClick={toggleSidebar}>
+                            <button className="text-black dark:text-white focus:outline-none" onClick={toggleSidebar}>
                                 {isSidebarOpen ? <ArrowLeftToLine className="w-6 h-5" /> : <ArrowRightFromLine className="w-6 h-5" />}
                             </button>
                         </div>
                         <div className="flex items-center">
                             <button
-                                className={`focus:outline-none rounded-full hover:bg-purple-600 hover:text-white p-1 mr-4 ${isNotificationDropdownOpen ? 'bg-[#836bfa] text-white' : 'text-gray-400'}`}
+                                className={`focus:outline-none rounded-full hover:bg-black hover:text-white p-1 mr-4 ${isNotificationDropdownOpen ? 'bg-black text-white' : ''}`}
                                 onClick={toggleNotificationDropdown}
                             >
                                 <Bell className="w-6 h-6" />
@@ -199,25 +199,25 @@ const Layout = ({ children }) => {
                                     </div>
                                 </div>
                             )}
-                            <button className="text-gray-600 dark:text-gray-400 focus:outline-none mr-4" onClick={toggleTheme}>
+                            <button className="text-black dark:text-white focus:outline-none mr-4" onClick={toggleTheme}>
                                 {theme === 'light' ? <Moon className="w-6 h-6" /> : <Sun className="w-6 h-6 text-yellow-500" />}
                             </button>
-                            <button className="text-gray-600 dark:text-gray-400 focus:outline-none mr-4" onClick={toggleFullscreen}>
+                            <button className="text-black dark:text-white focus:outline-none mr-4" onClick={toggleFullscreen}>
                                 {isFullscreen ? <Minimize2 className="w-6 h-6" /> : <Maximize2 className="w-6 h-6" />}
                             </button>
                             <div className="relative">
                                 <button
                                     onClick={toggleAvatarDropdown}
-                                    className="focus:outline-none rounded-full border-2 border-gray-300 bg-red-500 text-white w-10 h-10 flex items-center justify-center"
+                                    className="focus:outline-none rounded-full border-2 border-black bg-red-500 text-white w-10 h-10 flex items-center justify-center"
                                 >
                                     H
                                 </button>
                                 {isAvatarDropdownOpen && (
-                                    <div className={`absolute right-0 mt-2 w-36 bg-gray-100 dark:text-black dark:bg-gray-100 shadow-lg rounded-lg py-2`}>
-                                        <Link to="/profile" className="block px-4 py-2 text-gray-700 dark:text-black hover:bg-gray-100 dark:hover:bg-[#836bfa] dark:hover:text-white">
+                                    <div className={`absolute right-0 mt-2 w-36 bg-white dark:text-black shadow-lg rounded-lg py-2`}>
+                                        <Link to="/profile" className="block px-4 py-2  dark:text-black hover:bg-black hover:text-white dark:hover:bg-black dark:hover:text-white">
                                             Profile
                                         </Link>
-                                        <button onClick={handleLogout} className="block w-full text-left px-4 py-2 text-gray-700 dark:text-black hover:bg-gray-100 dark:hover:bg-[#836bfa] dark:hover:text-white">
+                                        <button onClick={handleLogout} className="block w-full text-left px-4 py-2 dark:text-black hover:bg-black hover:text-white dark:hover:bg-black dark:hover:text-white">
                                             Logout
                                         </button>
                                     </div>
@@ -233,7 +233,7 @@ const Layout = ({ children }) => {
             {showScrollButton && (
                 <button
                     onClick={scrollToTop}
-                    className="fixed bottom-5 right-5 bg-[#836bfa] text-white p-2 rounded-full hover:bg-purple-600 transition-all animate-bounceUpDown"
+                    className="fixed bottom-5 right-5 bg-black text-white p-2 rounded-full hover:bg-black transition-all animate-bounceUpDown"
                 >
                     <ArrowUp />
                 </button>
